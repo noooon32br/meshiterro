208b1122d5b071cf549a72e64d50fa5ef97fedb9
EC2 Default User <ec2-user@ip-172-31-26-236.ap-northeast-1.compute.internal>
2022-11-15 13:47:53 +0000

    devise設定変更

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,7 +4,7 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  validates :shopname, presence: true
+  validates :shop_name, presence: true
   validates :image, presence: true
 
   def get_image
